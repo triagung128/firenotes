6053c56702b1c7f97a6cb486ac3b2aa94fe7cc85
triagung128 <triagung128@gmail.com>
2022-09-06 12:28:08 +0700

    initial commit

---
+++ android/app/src/main/kotlin/com/example/firenotes/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.firenotes
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/app/routes/app_pages.dart
@@ -0,0 +1,20 @@
+import 'package:get/get.dart';
+
+import '../modules/home/bindings/home_binding.dart';
+import '../modules/home/views/home_view.dart';
+
+part 'app_routes.dart';
+
+class AppPages {
+  AppPages._();
+
+  static const initial = Routes.home;
+
+  static final routes = [
+    GetPage(
+      name: _Paths.home,
+      page: () => const HomeView(),
+      binding: HomeBinding(),
+    ),
+  ];
+}
