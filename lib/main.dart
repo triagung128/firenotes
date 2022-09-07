187f6efdc5fa0b7e6c40bb1459b1b1d9a8cb218a
triagung128 <triagung128@gmail.com>
2022-09-07 10:17:03 +0700

    set up session

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'firebase_options.dart';
 
 import 'app/routes/app_pages.dart';
+import 'app/modules/splash_screen/splash_screen.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -12,11 +14,26 @@ void main() async {
     options: DefaultFirebaseOptions.currentPlatform,
   );
 
+  FirebaseAuth auth = FirebaseAuth.instance;
+
   runApp(
-    GetMaterialApp(
-      title: "Application",
-      initialRoute: AppPages.initial,
-      getPages: AppPages.routes,
+    StreamBuilder<User?>(
+      stream: auth.authStateChanges(),
+      builder: (context, snapshot) {
+        if (snapshot.connectionState == ConnectionState.waiting) {
+          return const SplashScreen();
+        }
+
+        return GetMaterialApp(
+          title: "Firenotes",
+          debugShowCheckedModeBanner: false,
+          initialRoute:
+              snapshot.data != null && snapshot.data!.emailVerified == true
+                  ? Routes.home
+                  : Routes.login,
+          getPages: AppPages.routes,
+        );
+      },
     ),
   );
 }
